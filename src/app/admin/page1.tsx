25c169013d42d28120b6aefc0a7ed4ba339cc11f
aguilared <arquimedesgui@gmail.com>
2025-01-02 09:56:03 -0400

    imgs, form onsubmit, queryvacas

--- src/app/admin/page1.tsx
+++ src/app/admin/page1.tsx
@@ -35,15 +35,18 @@ const queryClient = new QueryClient({
 });
 
 type Inputs = {
-  author: { name: string };
-  author_id: number;
-  bitacora_date: string;
-  created_date: string;
+  alive: string;
+  birthdate: string;
+  clase_id: number;
+  hierro: string;
   id: number;
+  info: string;
+  mother: string;
+  mother_id: number;
+  name: string;
+  owner_id: number;
+  tipopart: string;
   updated_at: string;
-  _count: {
-    bita_events: number;
-  };
 };
 
 const dateBitacora = new Date();
@@ -91,7 +94,6 @@ const Bitacoras = (): React.JSX.Element => {
   const router = useRouter();
 
   const { isUser } = useUser();
-  const [datafilter, setDatafilter] = useState<Inputs[]>([]);
 
   useEffect(() => {
     if (!isUser) {
@@ -100,30 +102,15 @@ const Bitacoras = (): React.JSX.Element => {
     }
   }, [isUser, router]);
 
-  const { data, isLoading, refetch, error, status } = useQuery({
+  const { data, isLoading, refetch } = useQuery({
     queryKey: ["Bitacorass"],
-    queryFn: async (): Promise<Inputs[]> => {
+    queryFn: async () => {
       const data = await axios.get(`${DATABASEURL}bitacora`);
       return data.data;
     },
     refetchInterval: intervalMs,
   });
   console.log("DATAbitacoras", data);
-
-  useEffect(() => {
-    if (status === "success") {
-      console.log("====================================");
-      console.log("renders");
-      console.log("====================================");
-      setDatafilter(data);
-    }
-  }, [data, status]);
-
-  const [bitacoraSearch, setBitacoraSearch] = useState();
-  const handleSearchOnChange = (e) => {
-    console.log("value", e.target.value);
-    setBitacoraSearch(e.target.value);
-  };
 
   const [modalCreate, setModalCreate] = React.useState(false);
   const modalCreateOpen = () => setModalCreate(true);
@@ -257,66 +244,11 @@ const Bitacoras = (): React.JSX.Element => {
     }
   };
 
-  const searchs = () => {
-    const value = bitacoraSearch;
-    console.log("Value", value);
-    if (!value || value === "") {
-      return setDatafilter(data); //retorna a la data original
-    }
-    //filtrando la data al hacer un search
-    console.log("Entrando", value);
-
-    const newData: Inputs[] = data.filter(
-      (bitacora: Inputs) => bitacora.id === Number(value)
-    );
-    console.log("NEWDATA", newData);
-
-    setDatafilter(newData);
-  };
-
-  if (error) return <div>Error: {error.message}</div>;
-
   return (
     <Container>
       <div className="flex rounded items-center justify-between flex-wrap bg-gray-500">
         <div className="flex-grow text-left text-gray-100 px-3 py-1 m-2 ">
           {" Admin Bitacoras"}
-        </div>
-        <div>
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              searchs();
-            }}
-          >
-            <input
-              className="rounded py-2 px-4"
-              type="text"
-              placeholder="Search"
-              defaultValue=""
-              onChange={handleSearchOnChange}
-            />
-            <button
-              type="submit"
-              onClick={() => searchs()}
-              className="absolute w-10 h-10 rounded-full inline p-2 shadow"
-            >
-              {" "}
-              <svg
-                className="text-gray-100 w-6 h-6 fill-current"
-                viewBox="0 0 56.966 56.966"
-                xmlns="http://www.w3.org/2000/svg"
-                version="1.1"
-                id="Capa_1"
-                x="0px"
-                y="0px"
-                width="512px"
-                height="512px"
-              >
-                <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
-              </svg>
-            </button>
-          </form>
         </div>
         <div className="flex-grow text-right px-3 py-1 m-2 text-gray-100">
           <Button color="success" variant="contained" onClick={modalCreateOpen}>
@@ -339,8 +271,8 @@ const Bitacoras = (): React.JSX.Element => {
           <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900" />
         </div>
       ) : null}
-      {datafilter && datafilter?.length > 0
-        ? datafilter?.map((bitacora: any) => (
+      {data && data?.length > 0
+        ? data?.map((bitacora: any) => (
             <div
               className="flex rounded mb-1 shadow bg-slate-100 dark:bg-slate-800"
               key={bitacora.id}
--- src/pages/api/animals/rawQueryVacas.js
+++ src/pages/api/animals/rawQueryVacas.js
@@ -9,14 +9,16 @@ export default async function handle(req, res) {
     totalVacasAngel,
     totalVacasYoel,
     totalVacasCruz,
+    totalVacasPetra,
   ] = await prisma.$transaction([
-    prisma.$queryRaw`SELECT id,name, owner_id  FROM animals WHERE clase_id =1 ORDER BY owner_id ASC`,
-    prisma.$executeRaw`SELECT * FROM animals WHERE clase_id =1`,
-    prisma.$executeRaw`SELECT * FROM animals WHERE clase_id =1 AND owner_id= 1`,
-    prisma.$executeRaw`SELECT * FROM animals WHERE clase_id =1 AND owner_id= 2`,
-    prisma.$executeRaw`SELECT * FROM animals WHERE clase_id =1 AND owner_id= 3`,
-    prisma.$executeRaw`SELECT * FROM animals WHERE clase_id =1 AND owner_id= 4`,
-    prisma.$executeRaw`SELECT * FROM animals WHERE clase_id =1 AND owner_id= 5`,
+    prisma.$queryRaw`SELECT id,name, owner_id  FROM animals WHERE clase_id =1 AND live = true ORDER BY owner_id ASC`,
+    prisma.$executeRaw`SELECT * FROM animals WHERE clase_id =1 AND live = true`,
+    prisma.$executeRaw`SELECT * FROM animals WHERE clase_id =1 AND owner_id= 1 AND live = true`,
+    prisma.$executeRaw`SELECT * FROM animals WHERE clase_id =1 AND owner_id= 2 AND live = true`,
+    prisma.$executeRaw`SELECT * FROM animals WHERE clase_id =1 AND owner_id= 3 AND live = true`,
+    prisma.$executeRaw`SELECT * FROM animals WHERE clase_id =1 AND owner_id= 4 AND live = true`,
+    prisma.$executeRaw`SELECT * FROM animals WHERE clase_id =1 AND owner_id= 5 AND live = true`,
+    prisma.$executeRaw`SELECT * FROM animals WHERE clase_id =1 AND owner_id= 6 AND live = true`,
   ]);
 
   res.json({
@@ -26,6 +28,7 @@ export default async function handle(req, res) {
     totalVacasAngel,
     totalVacasYoel,
     totalVacasCruz,
+    totalVacasPetra,
     vacas,
   });
 }
