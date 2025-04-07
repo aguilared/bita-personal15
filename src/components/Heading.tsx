285e3ec210d4ba41c6e645200d5c81cb16923e4a
aguilared <arquimedesgui@gmail.com>
2025-04-06 21:36:05 -0400

    up imgs5425

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 interface HeadingProps {
   title: string;
@@ -6,21 +6,17 @@ interface HeadingProps {
   center?: boolean;
 }
 
-const Heading: React.FC<HeadingProps> = ({ 
-  title, 
-  subtitle,
-  center
-}) => {
-  return ( 
-    <div className={center ? 'text-center' : 'text-start'}>
-      <div className="text-2xl font-bold">
-        {title}
+const Heading: React.FC<HeadingProps> = ({ title, subtitle, center }) => {
+  return (
+    <header className="mt-8 mb-12">
+      <div className={center ? "text-center" : "text-start"}>
+        <div className="rounded sm:p-3 md:py-3 lg:px-3 py-3 px-4 bg-gray-200 dark:bg-black">
+          <div className="text-7xl font-bold">{title}</div>
+          <div className="font-light text-neutral-500 mt-2">{subtitle}</div>
+        </div>
       </div>
-      <div className="font-light text-neutral-500 mt-2">
-        {subtitle}
-      </div>
-    </div>
-   );
-}
- 
+    </header>
+  );
+};
+
 export default Heading;
--- src/pages/vacas/page.tsx
+++ src/pages/vacas/page.tsx
@@ -2,6 +2,7 @@
 import { useState, BaseSyntheticEvent, useEffect } from "react";
 import type { NextPage } from "next";
 import Container from "@/components/Container";
+import Heading from "@/components/Heading";
 import { colourOptions } from "@/components/data";
 
 import {
@@ -144,160 +145,163 @@ const AnimalsCardQuery: NextPage = () => {
   };
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <div className="rounded sm:p-3 md:py-3 lg:px-3 py-3 px-4 bg-gray-200 dark:bg-black">
-        <div className="container mx-auto px-20 text-gray-600 text-2xl font-bold">
-          Query List Animals{" "}
-        </div>
-
-        <div className="flex mb-4">
-          <div className="w-1/2 p-2 text-center bg-green-400">
-            {" "}
-            <Controller
-              name="owner_id"
-              control={control}
-              rules={{ required: true }}
-              render={({ field: { onChange, value, name, ref } }) => {
-                return (
-                  <Select
-                    defaultValue={{ label: "Seleccione..", value: 0 }}
-                    options={owners}
-                    name={name}
-                    onChange={(val?) => {
-                      console.log("Valuee Selected", val);
-                      onChange(val!.value);
-                      setOwnerId(val!.value);
-                      handleOnChange("owner_id", val!.value);
-                      handleSearchOnChange("owner_id", val!.value);
-                    }}
-                    onBlur={() => searchs()}
-                  />
-                );
-              }}
-            />
+    <main className="h-full">
+      <QueryClientProvider client={queryClient}>
+        <Heading title="Query List Animalsss" subtitle="Vacas" />
+        <div className="container rounded sm:p-3 md:py-3 lg:px-3 py-3 px-4 bg-gray-200 dark:bg-black">
+          <div className="mx-auto px-20 text-gray-600 text-2xl font-bold">
+            Query List Animals{" "}
           </div>
-          <div className="w-1/2 p-2 text-left bg-green-500">
-            <button
-              type="submit"
-              onClick={() => searchs()}
-              className="absolute w-10 h-10 rounded-full inline p-2 shadow"
-            >
+
+          <div className="flex mb-4">
+            <div className="w-1/2 p-2 text-center bg-green-400">
               {" "}
-              <svg
-                className="text-gray-100 w-6 h-6 fill-current"
-                viewBox="0 0 56.966 56.966"
-                xmlns="http://www.w3.org/2000/svg"
-                version="1.1"
-                id="Capa_1"
-                x="0px"
-                y="0px"
-                width="512px"
-                height="512px"
+              <Controller
+                name="owner_id"
+                control={control}
+                rules={{ required: true }}
+                render={({ field: { onChange, value, name, ref } }) => {
+                  return (
+                    <Select
+                      defaultValue={{ label: "Seleccione..", value: 0 }}
+                      options={owners}
+                      name={name}
+                      onChange={(val?) => {
+                        console.log("Valuee Selected", val);
+                        onChange(val!.value);
+                        setOwnerId(val!.value);
+                        handleOnChange("owner_id", val!.value);
+                        handleSearchOnChange("owner_id", val!.value);
+                      }}
+                      onBlur={() => searchs()}
+                    />
+                  );
+                }}
+              />
+            </div>
+            <div className="w-1/2 p-2 text-left bg-green-500">
+              <button
+                type="submit"
+                onClick={() => searchs()}
+                className="absolute w-10 h-10 rounded-full inline p-2 shadow"
               >
-                <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
-              </svg>
-            </button>
+                {" "}
+                <svg
+                  className="text-gray-100 w-6 h-6 fill-current"
+                  viewBox="0 0 56.966 56.966"
+                  xmlns="http://www.w3.org/2000/svg"
+                  version="1.1"
+                  id="Capa_1"
+                  x="0px"
+                  y="0px"
+                  width="512px"
+                  height="512px"
+                >
+                  <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
+                </svg>
+              </button>
+            </div>
+          </div>
+
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+            {data && data?.length > 0 ? (
+              datafilter?.map((animal: any) => (
+                <Card
+                  sx={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                  }}
+                  key={animal.id}
+                >
+                  <CardMedia>
+                    <div
+                      style={{
+                        position: "relative",
+                        width: "100%",
+                        height: "100%",
+                      }}
+                    >
+                      <Image
+                        src={"/static/images/" + `${animal.id}` + ".jpg"}
+                        width={520}
+                        height={400}
+                        alt=""
+                        style={{ objectFit: "cover" }}
+                      />
+                    </div>
+                  </CardMedia>
+                  <CardContent>
+                    <Typography align="left" component="div">
+                      <a
+                        href={`/animals/animalId/${encodeURIComponent(
+                          animal.id
+                        )}`}
+                        target={"_blank"}
+                        rel="noreferrer"
+                      >
+                        {" "}
+                        Animal ID: {animal.id},{" "}
+                      </a>
+                    </Typography>
+
+                    <Typography gutterBottom align="left">
+                      {animal.clase.description}: {animal.name}, Dueno:{" "}
+                      {animal.owner.name}
+                    </Typography>
+
+                    <Typography gutterBottom align="left">
+                      Nacimiento: {convertDate(animal.birthdate)}, Live:{" "}
+                      {animal.live! ? (
+                        <input
+                          type="checkbox"
+                          checked
+                          placeholder="Live"
+                          className="mx-3"
+                          onChange={() => console.log("checked")}
+                        />
+                      ) : (
+                        <input
+                          type="checkbox"
+                          placeholder="Live"
+                          className="mx-3"
+                        />
+                      )}{" "}
+                    </Typography>
+                    <Typography gutterBottom align="left">
+                      Tipo animal: <b> {animal.clase.description}</b>
+                    </Typography>
+
+                    <Typography align="left">
+                      Madre: {animal.mother},{" "}
+                      <a
+                        className="bg-blue-200 rounded underline hover:underline hover:underline-offset-4"
+                        href={`/animals/animal/4?id=${encodeURIComponent(
+                          animal.mother_id
+                        )}`}
+                        target={"_blank"}
+                        rel="noreferrer"
+                      >
+                        {" "}
+                        motherID:&nbsp; {animal.mother_id},{" "}
+                      </a>{" "}
+                    </Typography>
+
+                    <Typography paragraph>Info: {animal.info}</Typography>
+                    <Typography align="left">
+                      Date update: {convertDate(animal.updated_at)}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              ))
+            ) : (
+              <div className="fixed top-0 right-0 h-screen w-screen z-50 flex justify-center items-center">
+                <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900" />
+              </div>
+            )}
           </div>
         </div>
-
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-          {data && data?.length > 0 ? (
-            datafilter?.map((animal: any) => (
-              <Card
-                sx={{
-                  display: "flex",
-                  justifyContent: "space-between",
-                }}
-                key={animal.id}
-              >
-                <CardMedia>
-                  <div
-                    style={{
-                      position: "relative",
-                      width: "100%",
-                      height: "100%",
-                    }}
-                  >
-                    <Image
-                      src={"/static/images/" + `${animal.id}` + ".jpg"}
-                      width={520}
-                      height={400}
-                      alt=""
-                      style={{ objectFit: "cover" }}
-                    />
-                  </div>
-                </CardMedia>
-                <CardContent>
-                  <Typography align="left" component="div">
-                    <a
-                      href={`/animals/animalId/${encodeURIComponent(
-                        animal.id
-                      )}`}
-                      target={"_blank"}
-                      rel="noreferrer"
-                    >
-                      {" "}
-                      Animal ID: {animal.id},{" "}
-                    </a>
-                  </Typography>
-
-                  <Typography gutterBottom align="left">
-                    {animal.clase.description}: {animal.name}, Dueno:{" "}
-                    {animal.owner.name}
-                  </Typography>
-
-                  <Typography gutterBottom align="left">
-                    Nacimiento: {convertDate(animal.birthdate)}, Live:{" "}
-                    {animal.live! ? (
-                      <input
-                        type="checkbox"
-                        checked
-                        placeholder="Live"
-                        className="mx-3"
-                        onChange={() => console.log("checked")}
-                      />
-                    ) : (
-                      <input
-                        type="checkbox"
-                        placeholder="Live"
-                        className="mx-3"
-                      />
-                    )}{" "}
-                  </Typography>
-                  <Typography gutterBottom align="left">
-                    Tipo animal: <b> {animal.clase.description}</b>
-                  </Typography>
-
-                  <Typography align="left">
-                    Madre: {animal.mother},{" "}
-                    <a
-                      className="bg-blue-200 rounded underline hover:underline hover:underline-offset-4"
-                      href={`/animals/animal/4?id=${encodeURIComponent(
-                        animal.mother_id
-                      )}`}
-                      target={"_blank"}
-                      rel="noreferrer"
-                    >
-                      {" "}
-                      motherID:&nbsp; {animal.mother_id},{" "}
-                    </a>{" "}
-                  </Typography>
-
-                  <Typography paragraph>Info: {animal.info}</Typography>
-                  <Typography align="left">
-                    Date update: {convertDate(animal.updated_at)}
-                  </Typography>
-                </CardContent>
-              </Card>
-            ))
-          ) : (
-            <div className="fixed top-0 right-0 h-screen w-screen z-50 flex justify-center items-center">
-              <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900" />
-            </div>
-          )}
-        </div>
-      </div>
-    </QueryClientProvider>
+      </QueryClientProvider>
+    </main>
   );
 };
 
