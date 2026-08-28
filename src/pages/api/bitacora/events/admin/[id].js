40dc08dd85d13bdf1dd71fbf5eeaec24e6d2ec82
aguilared <arquimedesgui@gmail.com>
2026-08-28 13:29:37 -0400

    imgs 28826

--- src/pages/api/bitacora/events/admin/[id].js
+++ src/pages/api/bitacora/events/admin/[id].js
@@ -2,6 +2,7 @@ import prisma from "../../../../../lib/prisma";
 
 export default async function handle1(req, res) {
   const bitacoraId = req.query.id;
+  console.log("bitacoraId", bitacoraId);
   const [result] = await prisma.$transaction([
     prisma.bitaEvents.findMany({
       where: { bitacora_id: Number(bitacoraId) },
